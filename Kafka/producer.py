20264ce65ed03df139c6d7d02118a83f780c2007
Farhan Hormasji <farhanh23@gmail.com>
2017-01-25 11:26:11 -0800

    added initial kafka producer code

--- Kafka/producer.py
+++ Kafka/producer.py
@@ -1,35 +1,12 @@
-import os
-import sys
-from confluent_kafka import Producer
-from random import random
+import kafka
+import json
 
-p = Producer({'bootstrap.servers': '{}'.format(os.environ['KAFKA_BROKERS'])})
+cluster = kafka.KafkaClient("35.166.234.119:9092")
+#producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = kafka.SimpleProducer(cluster)
+jsonFile = open("sample.json", 'r')
 
+for line in jsonFile:
+	#producer.send('my-topic',line)
+	producer.send_messages('my-topic', line)
 
-
-def delivery_callback (err, msg):
-    if err:
-        sys.stderr.write('%% Message failed delivery: %s\n' % err)
-    else:
-        #sys.stderr.write('%% Message delivered to %s [%d]\n' % \
-        #                 (msg.topic(), msg.partition()))
-        pass
-
-# Produce each line to Kafka
-while True:
-    msg = "{}, some message for kafka".format(int(random()*1000000))
-    try:
-        # Produce line (without newline)
-        p.produce("messages", msg.encode('utf-8'), callback=delivery_callback)
-        
-    except BufferError as e:
-        sys.stderr.write('%% Local producer queue is full ' \
-                         '(%d messages awaiting delivery): try again\n' %
-                         len(p))
-        p.flush()
-
-    # Serve delivery callback queue.
-    # NOTE: Since produce() is an asynchronous API this poll() call
-    #       will most likely not serve the delivery callback for the
-    #       last produce()d message.
-    p.poll(0)

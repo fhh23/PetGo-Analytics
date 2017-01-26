c019f007eca3e4faa2579fc0f187bf008921a4e9
Farhan Hormasji <farhanh23@gmail.com>
2017-01-26 15:44:11 -0800

    Added code to write to s3 in Data Gen folder. Added code to read from s3 in Kafka producer file

--- Data Gen/Data_Generator.py
+++ Data Gen/Data_Generator.py
@@ -1,10 +1,20 @@
 import json
-sample = {'time': 12417,  
-          'item': 1675, 
-          'bought': 1}
-d = {"name":"user1",
-     "children":[{'name':key,"size":value} for key,value in sample.items()]}
-j = json.dumps(d, indent=4)
-f = open('../Kafka/sample.json', 'w')
-print(j,file=f)
-f.close()
+import boto3
+
+s3_client = boto3.client('s3')
+open('hello.txt', 'w').write("To be, or not to be,--that is the question:--" + 
+      "Whether 'tis nobler in the mind to suffer" + "The slings and arrows of outrageous fortune" +
+      "Or to take arms against a sea of troubles")
+
+# Upload the file to S3
+s3_client.upload_file('hello.txt', 'fh-data-insight', 'hello-remote.txt')
+		
+# sample = {'time': 12417,  
+          # 'item': 1675, 
+          # 'bought': 1}
+# d = {"name":"user1",
+     # "children":[{'name':key,"size":value} for key,value in sample.items()]}
+# j = json.dumps(d, indent=4)
+# f = open('../Kafka/sample.json', 'w')
+# print(j,file=f)
+# f.close()
--- Kafka/producer.py
+++ Kafka/producer.py
@@ -1,12 +1,44 @@
 import kafka
 import json
+import boto3
+import time
+
+#s3_client.download_file('MyBucket', 'hello-remote.txt', 'hello2.txt')
+def get_s3_bucket(bucket_name):
+	s3 = boto3.resource('s3')
+	try:
+		s3.meta.client.head_bucket(Bucket=bucket_name)
+	except botocore.exceptions.ClientError as e:
+		# If a client error is thrown, check that it was a 404 error.
+		# If it was a 404 error, then the bucket does not exist.
+		error_code = int(e.response['Error']['Code'])
+		if error_code == 404:
+			exists = False
+			print (e.response['404 Error: bucket not found'])
+		else:
+			print (e.response['Error'])
+		return None
+	else:
+		return s3.Bucket(bucket_name)
 
 cluster = kafka.KafkaClient("35.166.234.119:9092")
 #producer = KafkaProducer(bootstrap_servers='localhost:9092')
 producer = kafka.SimpleProducer(cluster)
-jsonFile = open("sample.json", 'r')
 
-for line in jsonFile:
-	#producer.send('my-topic',line)
-	producer.send_messages('my-topic', line)
+bucket_name = 'fh-data-insight'
+bucket = get_s3_bucket(bucket_name)
+for obj in bucket.objects.limit(1):
+	obj_body = obj.get()['Body']
+	json_body = obj_body.read()
+	producer.send_messages('my-topic', json_body)
+	time.sleep(5)
+	print json_body
+
+
+# #jsonFile = open("sample.json", 'r')
+# textFile = open('hello2.txt')
+
+# for line in jsonFile:
+	# #producer.send('my-topic',line)
+	# producer.send_messages('my-topic', line)
 

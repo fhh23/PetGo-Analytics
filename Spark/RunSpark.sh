40302f6f3c83e550c73ca6ea1112e449819c38c8
Ubuntu <ubuntu@ip-172-31-0-164.us-west-2.compute.internal>
2017-01-30 22:54:03 +0000

    added script to run spark file. Edited kafka stream info in SparkStreaming.py

---
+++ Spark/RunSpark.sh
@@ -0,0 +1,2 @@
+python ../Kafka/producer.py  
+/usr/local/spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.0 SparkStreaming.py
--- Spark/SparkStreaming.py
+++ Spark/SparkStreaming.py
@@ -1,20 +1,29 @@
+#!/usr/bin/env pyspark
+
+from __future__ import print_function
+
 import os
 import pyspark
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import pprint
 
+from pyspark import SparkContext, SparkConf
+sc = SparkContext(appName='streamingFromKafka')
+ssc = StreamingContext(sc, 2)
 # Set the Kafka topic
-topic = "twitter_stream_new"
+topic = 'fh-topic'
 
 # List the Kafka Brokers
-broker_file = open('kafka_brokers.txt', 'r')
+broker_file = open('brokers.txt', 'r')
 kafka_brokers = broker_file.read()[:-1]
 broker_file.close()
-kafkaBrokers = {"metadata.broker.list": kafka_brokers}
+kafkaBrokers = {"metadata.broker.list": "ec2-35-166-31-140.us-west-2.compute.amazonaws.com:9092"}
 
 # Create input stream that pull messages from Kafka Brokers (DStream object)
 tweets_raw = KafkaUtils.createDirectStream(ssc, [topic], kafkaBrokers)
-
 # Printing kafkastream content 
-kafka_stream.pprint()
+tweets_raw.pprint()
+
+ssc.start()
+ssc.awaitTermination()

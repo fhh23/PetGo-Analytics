9b49357b34ecd31a4c25a8726b6d20ce600b750c
Ubuntu <ubuntu@ip-172-31-0-164.us-west-2.compute.internal>
2017-02-05 11:26:59 +0000

    have apriori algorithm working in SparkStreaming.py

--- Spark/FreqItemsetTest.py
+++ Spark/FreqItemsetTest.py
@@ -1,9 +1,95 @@
+from collections import defaultdict
+from itertools import imap, combinations
 import sys
 import copy
-
+import apriori
 from string import atoi
 from pyspark import SparkContext, SparkConf
-import apriori
+import Itemsets 
+
+def get_frequent_items_sets(data,min_sup,steps=0):
+
+	# we transform the dataset in a list of sets
+	transactions = list()
+
+	# Temporary dictionary to count occurrences
+	items = defaultdict(lambda: 0)
+
+	# Returned dictionary
+	solution = dict()
+	L_set = set()
+
+	# Fills transactions and counts "singletons"
+	for line in data:
+		transaction = set(line)
+		transactions.append(transaction)
+		for element in transaction:
+			items[element]+=1
+
+	# Add to the solution all frequent items
+	for item, count in items.iteritems():
+		if count >= min_sup:
+			L_set.add(frozenset([item]))
+
+	# Generalize the steps it ends when there are no candidates
+	# or if the user provided as input the number of parameters
+	k = 2
+	solution[k-1] = L_set
+	while L_set != set([]) and k != steps+1:
+		L_set = create_candidates(L_set,k)
+		C_set = frequent_items(L_set,transactions,min_sup)
+		if C_set != set([]): solution[k]=C_set
+		L_set = C_set
+		k = k + 1
+	return solution
+
+
+
+
+
+# Creates candidates joining the same set
+# input:
+#    itemSet: a set of frequent items
+#     length: the cardinality of generated combinations
+#  returns:
+#	   set: a set containing all combinations with cardinality equal to length
+#		(be carefull on iterating on it)
+
+def create_candidates(itemSet, length):
+        return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
+
+
+
+
+# Checks occurrences of items in transactions and returns frequent items
+# input:
+#    items: a set of frequent items (candidates)
+#     transactions: list of sets representing baskets
+#  returns:
+#	   _itemSet: a set containing all frequent candidates (a subset of inputs)
+
+def frequent_items(items, transactions, min_sup):
+	_itemSet = set()
+	counter = defaultdict(int)
+	localDict = defaultdict(int)
+	for item in items:
+		for transaction in transactions:
+			if item.issubset(transaction):localDict[item] += 1
+
+	for item, count in localDict.items():
+		if count >= min_sup:
+			_itemSet.add(item)
+	return _itemSet
+
+
+# helper to standardize the output (not part of the algorithm)
+
+def please_clean(solution):
+	res = []
+	for i in solution.itervalues():
+		for j in i:
+			res.append(j)
+	return res
 
 def findFrequentItemsets(input, output, numPartitions, s, sc):
     """
@@ -33,12 +119,12 @@ def findFrequentItemsets(input, output, numPartitions, s, sc):
 
     #split string baskets into lists of items
     baskets = data.map(lambda line: sorted([int(y) for y in line.strip().split(' ')]))
-
+    print(baskets.collect())
     #treat a basket as a set for fast check if candidate belongs
     basketSets = baskets.map(set).persist()
 
     #each worker calculates the itemsets of his partition
-    localItemSets = baskets.mapPartitions(lambda data: [x for y in apriori.get_frequent_items_sets(data, threshold/numPartitions).values() for x in y], True)
+    localItemSets = baskets.mapPartitions(lambda data: [x for y in get_frequent_items_sets(data, threshold/numPartitions).values() for x in y], True)
 
     #for reducing by key later
     allItemSets = localItemSets.map(lambda n_itemset: (n_itemset,1))

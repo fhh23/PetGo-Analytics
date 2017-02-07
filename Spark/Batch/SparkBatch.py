429a77603d4fe0706ac8c82d80c4d937db7a503d
Ubuntu <ubuntu@ip-172-31-0-164.us-west-2.compute.internal>
2017-02-07 23:48:34 +0000

    fixed connection to s3 for batch, fixed include of common file import (itemsets.py)

--- Spark/Batch/SparkBatch.py
+++ Spark/Batch/SparkBatch.py
@@ -8,26 +8,41 @@ import pprint
 from pyspark import SparkContext, SparkConf
 from operator import add
 import sys
-import ../common/Itemsets
+import boto
 
 percentile_broadcast = None
+
+def splitFunc(rdd):
+    import Itemsets
+    Itemsets.lineSplit(rdd)
 
 sc = SparkContext(appName='ItemsetsBatch')
 
 # Read in from S3
 aws_access_key = os.getenv('AWS_ACCESS_KEY_ID', 'default')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY', 'default')
-val data = sc.textFile("s3n://aws_access_key:aws_secret_access_key@/fh-data-insight/*")
-x.take(5)
 
+conn = boto.connect_s3(aws_access_key, aws_secret_access_key)
+bucket = conn.get_bucket('fh-data-insight')
+data = sc.testFile("s3n://fh-data-insight/*")
+print(data.take(5)
+
+bucket_path = "s3n://fh-data-insight/"
+for f in bucket:
+    rdd = sc.textFile(bucket_path + f.name)
+#    print(rdd.take(5))
+
+
+
+print("HI \n")
+'''
 body = data.map(lambda x: x[1])
 lines = body.flatMap(lambda bodys: bodys.split("\r\n")) 
-word = lines.map(lineSplit) \
+word = lines.map(splitFunc) \
             .map(lambda word: (word, 1)) \
             .reduceByKey(lambda a, b: a+b)
             
-print_word = word.pprint()
-
+print_word = word.take(5)
 numPartitions = 1 
 s = .3
 #count = lines.count()
@@ -40,6 +55,7 @@ baskets = lines.map(Itemsets.lineSplit2) \
                .mapValues(list) \
                .map(lambda x: sorted(x[1]))
 print("RDD: \n")
+'''
 '''
 #treat a basket as a set for fast check if candidate belongs
 basketSets = baskets.map(set).cache()
@@ -83,5 +99,3 @@ finalItemSets.saveAsTextFiles("spark_out.txt")
 #print(digest.percentile(50), file=f)
 #print("\n", file=f)
 '''
-ssc.start()
-ssc.awaitTermination()
